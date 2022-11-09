7e7f37addb6c4382de61b6d382dea62ab2a4dcb4
Tal <tale1000@gmail.com>
2022-11-09 18:31:23 +0200

    add side-bar cmp + mixin 'page' that contains the style of the pages in the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app-root/app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PostAppComponent } from './cmps/post-app/post-app.component';
 import { PostListComponent } from './cmps/post-list/post-list.component';
 import { PostPreviewComponent } from './cmps/post-preview/post-preview.component';
+import { SideBarComponent } from './cmps/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +15,13 @@ import { PostPreviewComponent } from './cmps/post-preview/post-preview.component
     HomeComponent,
     PostAppComponent,
     PostListComponent,
-    PostPreviewComponent
+    PostPreviewComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
