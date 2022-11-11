f63e5cd62fea6d7b6dce39fd43191849a80a2799
Tal <tale1000@gmail.com>
2022-11-11 09:18:08 +0200

    add storage service

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+
+  public saveToStorage<T>(key: string, value: T) {
+    localStorage.setItem(key, JSON.stringify(value))
+  }
+  public loadFromStorage(key: string) {
+    let data = localStorage.getItem(key)
+    return data ? JSON.parse(data) : undefined
+  }
 }
