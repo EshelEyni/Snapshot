297143857c5b18f30b38bf24f98ccdfc4637564e
Tal <tale1000@gmail.com>
2022-11-11 09:29:28 +0200

    add option to save posts to local storage

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,3 +1,4 @@
+import { StorageService } from './storage.service';
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject, of, throwError } from 'rxjs';
 import { Post } from '../models/post.model';
@@ -11,7 +12,7 @@ const POSTS = [
     location: { lat: 32.0749831, lng: 34.9120554, name: 'Tel Aviv' },
     likedBy: [{ _id: '132', fullname: 'User 1', imgUrl: 'https://res.cloudinary.com/dng9sfzqt/image/upload/v1664955076/ifizwgsan7hjjovf2xtn.jpg' }],
     commentsIds: ['1asdasd', '2asdasd', '3asdasd'],
-    createdAt: new Date(	2022,	10,9,	22,22,	59,	0),
+    createdAt: new Date(2022, 10, 9, 22, 22, 59, 0),
     tags: ['tag1', 'tag2']
   },
   {
@@ -34,11 +35,18 @@ const POSTS = [
 export class PostService {
 
   private _postsDb: Post[] = POSTS;
+
   private _posts$ = new BehaviorSubject<Post[]>([]);
   public posts$ = this._posts$.asObservable();
-  constructor() { }
+
+  constructor(private storageService: StorageService) { }
 
   public loadPosts(): void {
-    this._posts$.next(this._postsDb);
+    let posts = this.storageService.loadFromStorage('post') || null
+    if (!posts) {
+      posts = this._postsDb;
+      this.storageService.saveToStorage('post', posts)
+    }
+    this._posts$.next(posts);
   }
 }
